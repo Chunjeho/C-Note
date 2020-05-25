4572dc939f5fcaa78fb72fbfea8e0c1dc10dff9c
Chunjeho <jejehoho3317@gmail.com>
2020-05-26 01:11:30 +0900

    Add files via upload

---
+++ Etc/Preprocessor.c
@@ -0,0 +1,46 @@
+/*
+	Use preprocesser in simple way.
+	It's not smart as you thought.
+*/
+
+#include <stdio.h>
+#include "pre.h" // User path
+
+// #define <name> <value>
+#define PI 3.141592 // constant
+#define E 2.718281828
+#define DEBUG 0
+#define OS 5
+
+int main(){
+	
+	int num = 0;
+	
+	num = add(2,3); //function which is in pre.h
+	
+	printf("pi: %lf\n", PI);
+	printf("e: %lf\n", E);
+	printf("sum: %d\n", num);
+	
+#ifdef DEBUG //if DEBUG is predefined, execute below code
+	printf("Debug-mode: yes\n");
+#else
+	printf("Debug-mode: no\n"); //else execute below code
+#endif //end
+	
+#if OS == 0 //we can use #if as if-statement in C
+	printf("Windows\n");
+#elif OS == 1 //we use #elif, not #elseif
+	printf("Linux\n");
+#elif OS == 2
+	printf("MacOS\n");
+#elif OS == 3
+	printf("IOS\n");
+#elif OS == 4
+	printf("Android\n");
+#else
+	printf("UNIX\n");
+#endif	
+
+	return 0;
+}
---
+++ Etc/pre.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int add(int, int);
+
+int add(int n, int m){
+	return n+m;
+}
