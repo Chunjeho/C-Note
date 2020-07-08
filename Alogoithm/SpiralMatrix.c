c5c9067980d6720ef580cd95a8306afadac1759f
Chunjeho <jejehoho3317@gmail.com>
2020-07-08 19:37:22 +0900

    Add files via upload

--- Alogoithm/SpiralMatrix.c
+++ Alogoithm/SpiralMatrix.c
@@ -54,13 +54,15 @@ void SpiralMatrix(int *_input, int m, int n, int **_bowl){
 			printf("_bowl[%d][%d] : %d\n", j, i, _bowl[j][i]);
 			_cont = (j == 0+_offset) ? _cont : _cont+1;
 			printf("_cont: %d\n", _cont);
+			if(_bowl[_offset+1][_offset] == _input[m*n-1]){
+				return;
+			} // There is no more extra element(s) inside this cycle <=> last element in _input array is in _bowl[_offset+1][_offset]
 		}
-		
 		_bowl[1+_offset][1+_offset] = _bowl[0+_offset][0+_offset];
 		_bowl[0+_offset][0+_offset] = _exc;
 		
 		if(_bowl[1+_offset][1+_offset] == _input[m*n-1]){
-			return;
+			return; // This will operate in M x M matrix
 		}
 		
 		_exc = _bowl[1+_offset][1+_offset];
@@ -71,7 +73,7 @@ void SpiralMatrix(int *_input, int m, int n, int **_bowl){
 
 int main(){
 	
-	int _input[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16, 17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36};
+	int _input[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36};
 	int m = 6;
 	int n = 6;
 	int i = 0;
