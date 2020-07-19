866fefe0595aed9305c11be6ec8e49f93a616fb7
Chunjeho <jejehoho3317@gmail.com>
2020-07-19 14:20:26 +0900

    Rename AvoidSensor.c to avoidSensor.c

--- Alogoithm/avoidSensor.c
+++ Alogoithm/avoidSensor.c
@@ -1,3 +1,8 @@
+/* 
+	0 is a motion detector. A range of the detector is 8 blocks surrrounded on 0.
+	Find the shortest distance (from left-end to right-end) avoiding motion detector. (You can go by up, down, left, right)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -27,16 +32,15 @@ int main(){
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	};*/
+	};*/ //another example
 	
 	int _Sensor[10][10];
 	
 	int *S = NULL;
 	int *F = NULL;
-	int *N = NULL;
 	
-	int _start = 0;
-	int _finish = 0;
+	int _start = 0; 
+	int _finish = 0; 
 	
 	int i = 0;
 	int j = 0;
@@ -136,34 +140,35 @@ int main(){
 	
 	for(i=0;i<=9;i++){
 		if(Sensor[i][0] == 1){
-			_start += 1;
+			_start += 1;//The number of start-point 
+
 		}
 		
 		if(Sensor[i][9] == 1){
-			_finish+= 1;
+			_finish+= 1; //The number of end-point
 		}
 	}
 	
-	S = malloc(sizeof(int) * _start);
-	F = malloc(sizeof(int) * _finish);
-	N = malloc(sizeof(int) * _start *_finish);
+	S = malloc(sizeof(int) * _start); 
+	F = malloc(sizeof(int) * _finish); 
 	
 	j = 0;
 	k = 0;
 	
 	for(i=0;i<=9;i++){
 		if(Sensor[i][0] == 1){
-			S[j] = i;
+			S[j] = i; //Collect start-point index
 			j += 1;
 		}
 		
 		if(Sensor[i][9] == 1){
-			F[k] = i;
+			F[k] = i; //Collect end-point index
 			k += 1;
 		}
 	}
 	
 	for(k=0;k<_start;k++){
+		/* Reset for new k */
 		_boolFin = 0;
 		
 		for(i=0;i<10;i++){
@@ -171,8 +176,9 @@ int main(){
 				Sensor[i][j] = _Sensor[i][j];
 			}
 		}
+		/* Reset Finish */
 		
-		/* Cycle */
+		/* Cycle Start*/
 		for(j=0;j<10;j++){
 			if(Sensor[S[k]][j] == 1){
 				Sensor[S[k]][j] = j+1;
@@ -206,8 +212,9 @@ int main(){
 		
 		/* While start */
 		
-		while(_boolFin <= _finish){
+		while(_boolFin <= _finish){ //Check, if you reached to the all end-points.
 			
+			/* Direction: right */
 			for(i=0;i<=9;i++){
 				for(j=0;j<=8;j++){
 					if(Sensor[i][j+1] == 1 && (Sensor[i][j] != 1 && Sensor[i][j] != 0 && j != 0)){
@@ -216,6 +223,7 @@ int main(){
 				}
 			}
 		
+			/* Direction: up */
 			for(i=0;i<=8;i++){
 				for(j=0;j<=9;j++){
 					if(Sensor[i+1][j] == 1 && (Sensor[i][j] != 1 && Sensor[i][j] != 0 && j != 0)){
@@ -224,6 +232,7 @@ int main(){
 				}
 			}
 			
+			/* Direction: down */
 			for(i=9;i>=1;i--){
 				for(j=9;j>=0;j--){
 					if(Sensor[i-1][j] == 1 && (Sensor[i][j] != 1 && Sensor[i][j] != 0 && j != 0)){
@@ -232,6 +241,7 @@ int main(){
 				}
 			}
 			
+			/* Direction: left */
 			for(i=9;i>=0;i--){
 				for(j=9;j>=1;j--){
 					if(Sensor[i][j-1] == 1 && (Sensor[i][j] != 1 && Sensor[i][j] != 0 && j != 1)){
@@ -240,6 +250,7 @@ int main(){
 				}
 			}
 			
+			/*  add 1 to _boolFin, if you reached to the right-end correctly */
 			for(i=0;i<_finish;i++){
 				if(Sensor[F[i]][9] != 1){
 					_boolFin += 1;
@@ -247,16 +258,8 @@ int main(){
 			}
 		}
 		/* While End */
-		/* End */
 		
-		/*printf("\n _Sensor\n");
-		for(i=0;i<10;i++){
-			for(j=0;j<10;j++){
-				printf(" %d ", _Sensor[i][j]);
-			}
-			printf("\n");
-		}*/
-		
+		/* Print all distance at each k */
 		printf("\n #%d \n", S[k]);
 		for(i=0;i<10;i++){
 			for(j=0;j<10;j++){
@@ -264,7 +267,9 @@ int main(){
 			}
 			printf("\n");
 		}
+		/* Cycle End */
 		
+		/* Find Minimum distance Start*/
 		for(i=0;i<10;i++){
 			if(Sensor[i][9] != 0 && Sensor[i][9] != 1 && k == 0 && j >= 0){
 				min = Sensor[i][9];
@@ -279,9 +284,11 @@ int main(){
 				}
 			}
 		}
+		/* Find Minimum distance Finish*/
 	}
 	
 	free(S);
+	free(F);
 	
 	printf("\n Shortest distance: %d\n ", min-1);
 	
